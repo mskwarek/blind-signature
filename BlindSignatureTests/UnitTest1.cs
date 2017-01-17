0e535a7316c56952e11bb510cb22adfd15f863a9
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-01-17 16:13:42 -0500

    unit test added - template

---
+++ BlindSignatureTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blind_sign;
+
+namespace BlindSignatureTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Message msg = new Message();
+            msg.testc();
+        }
+    }
+}
